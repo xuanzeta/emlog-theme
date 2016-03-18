453da6fe9e4de9cbcea21874974b62b7060e2a5b
Liu Man <907999456@qq.com>
2016-03-18 20:36:53 +0800

    手机自适应

--- header.php
+++ header.php
@@ -13,6 +13,7 @@ require_once View::getView('module');
 <!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Transitional//EN" "http://www.w3.org/TR/xhtml1/DTD/xhtml1-transitional.dtd">
 <html xmlns="http://www.w3.org/1999/xhtml">
 <head>
+<meta name="viewport" content="width=device-width, initial-scale=1.0">
 <meta http-equiv="Content-Type" content="text/html; charset=UTF-8" />
 <title><?php echo $site_title; ?></title>
 <meta name="keywords" content="<?php echo $site_key; ?>" />
